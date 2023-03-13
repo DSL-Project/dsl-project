87334064c78e15dca58978db12ca6eab018892b5
AL <skininja673@gmail.com>
2023-03-13 15:06:39 -0400

    added pages and router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import Contact from "../../pages/Contact";
-import Footer from "../Footer";
+import { Routes, Route } from 'react-router-dom';
+import Projects from '../../pages/Projects';
+import Training from '../../pages/Training';
+import Publications from '../../pages/Publications';
+import People from '../../pages/People';
+import Contact from '../../pages/Contact';
+import Footer from '../Footer';
 
-import "../../App.scss";
+import '../../App.scss';
 // testing initial commit and add front-end branch
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path='/projects' element={<Projects />} />
+                <Route path='/training' element={<Training />} />
+                <Route path='/publications' element={<Publications />} />
+                <Route path='/people' element={<People />} />
+                <Route path='/contact' element={<Contact />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
