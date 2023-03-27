267c8b6b71f1cd7e019056ed98ac64d6da5f3810
ChristineWest <christinewwwest@gmail.com>
2023-03-26 20:57:13 -0400

    created and styled NavBar

--- src/components/App/SharedLayout.js
+++ src/components/App/SharedLayout.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../Header';
-import Footer from '../Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+//import Header from '../Header';
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer";
 
 const SharedLayout = () => {
-    return (
-        <>
-            <Header />
-            <Outlet />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default SharedLayout;
---
+++ src/components/NavBar/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavLinks from "./NavLinks";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="bold-18 logo">Digital Society Lab</div>
+      <NavLinks />
+    </div>
+  );
+};
+
+export default Navigation;
