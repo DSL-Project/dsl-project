739fe1ca324b3bf15d48004911a10c82e5ecc232
ChristineWest <christinewwwest@gmail.com>
2023-03-30 13:29:40 -0400

    Home - funder logo and email form

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,45 @@
-import Announcement from './Announcement';
-import { AiOutlineArrowDown } from 'react-icons/ai';
-import { FaFacebookF, FaTwitter } from 'react-icons/fa';
-import { Link } from 'react-scroll';
-import ProjectsCarousel from './ProjectsCarousel';
-import { useGlobalContext } from '../../appContext';
+import Announcement from "./Announcement";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { Link } from "react-scroll";
+import ProjectsCarousel from "./ProjectsCarousel";
+import { useGlobalContext } from "../../appContext";
+import React, { useState, useEffect } from "react";
+import defaultImg from "../../assets/defaultImg.jpg";
 
 function Home() {
-    const { homepageData: response } = useGlobalContext();
-    // console.log(response);
-    //! waiting for data from useGlobalContext()
+  const handleSubmit = () => {
+    window.open("https://buttondown.email/digitalsocietylab", "popupwindow");
+  };
+  const [isLoading, setIsLoadding] = useState(true);
+  const { homepageData } = useGlobalContext();
+  const homeStatic = homepageData[0];
+  useEffect(() => {
+    if (homeStatic) {
+      console.log(homeStatic.homeFunders[0]);
+      setIsLoadding(false);
+    }
+  }, [homeStatic]);
 
-    return (
-        <div className='home'>
-            {/* This is the announcement banner:
-      //TODO: 1. the link is clickable to a page called "Opportunities" which may need to be dynamically populated as hiring opportunities can change over time
-       */}
-            <Announcement />
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-            <div className='home-wrapper'>
-                <div className='welcome-container'>
-                    {/* <h1>{response[0].hometitle}</h1>  for now this renders Home not "Welcome to the Digital Society Lab*/}
-                    <h1>Welcome to the Digital Society Lab</h1>
-                    <p className='regular-16'>
-                        {response[0]?.homebody?.content[1]?.content[1] ??
-                            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+  return (
+    <div className="home">
+      {/* This is the announcement banner built and styled which can be taken out as per client's request*/}
+
+      <Announcement />
+
+      <div className="home-wrapper">
+        <div className="welcome-container">
+          <h1>
+            {homeStatic?.hometitle ?? `Welcome to the Digital Society Lab`}
+          </h1>
+          {/* <h1>Welcome to the Digital Society Lab</h1> */}
+          <p className="regular-16">
+            {homeStatic?.homebody?.content[1]?.content[1] ??
+              `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
             justo nunc, ornare eget rutrum at, tristique ut nibh. Phasellus in
             lobortis justo. Suspendisse ac dui sit amet elit porttitor
             elementum. Nulla a tincidunt arcu. Curabitur molestie lectus
@@ -33,9 +49,9 @@ function Home() {
             Donec pulvinar ultricies lectus, sed euismod ex consectetur et.
             Nullam rhoncus risus eros, a dignissim risus fringilla sed. Fusce
             semper ac sapien a pulvinar.`}
-                    </p>
+          </p>
 
-                    {/* //!Leave the hard coded content for now in for styling bug fixes
+          {/* //!Leave the hard coded content for now in for styling bug fixes
           <p className="regular-16">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
             justo nunc, ornare eget rutrum at, tristique ut nibh. Phasellus in
@@ -48,110 +64,119 @@ function Home() {
             Nullam rhoncus risus eros, a dignissim risus fringilla sed. Fusce
             semper ac sapien a pulvinar.
           </p> */}
-                    <div className='recent-projects'>
-                        <Link
-                            to='project-carousel'
-                            smooth={true}
-                            duration={500}
-                            className='bold-18'
-                        >
-                            See recent projects
-                        </Link>
-                        <AiOutlineArrowDown />
-                    </div>
-                </div>
-                <div id='project-carousel'>
-                    <ProjectsCarousel />
-                </div>
-                <div className='funding-container'>
-                    <h2>
-                        {response[0]?.homeFundersText ??
-                            `Made possible with funding from:`}
-                    </h2>
+          <div className="recent-projects">
+            <Link
+              to="project-carousel"
+              smooth={true}
+              duration={500}
+              className="bold-18"
+            >
+              See recent projects
+            </Link>
+            <AiOutlineArrowDown />
+          </div>
+        </div>
+        <div id="project-carousel">
+          <ProjectsCarousel />
+        </div>
+        <div className="funding-container">
+          <h2>
+            {homeStatic?.homeFundersText ?? `Made possible with funding from:`}
+          </h2>
 
-                    {/* //! leaving the hard coded content for now for styling bug fixes
+          {/* //! leaving the hard coded content for now for styling bug fixes
           <h2>Made possible with funding from:</h2> */}
 
-                    <div className='funding-logos'>
-                        {response[0]?.homeFunders?.map((funder) => (
-                            <div key={funder.sys.id} className='funder-logo'>
-                                <img
-                                    src={funder.fields.file.url}
-                                    alt={funder.fields.title}
-                                />
-                            </div>
-                        )) ?? (
-                            <div className='funder-logo'>
-                                Default funder logo when response is not
-                                available
-                            </div>
-                        )}
-                    </div>
+          <div className="funding-logos">
+            {homeStatic?.homeFunders?.map((funder) => (
+              <div key={funder.sys.id} className="funder-logo">
+                <img
+                  src={`https:${funder.fields.file.url}`}
+                  alt={funder.fields.title}
+                />
+              </div>
+            )) ?? (
+              <div className="funder-logo">
+                <img
+                  src={defaultImg}
+                  alt="a map indicating digital society lab location"
+                />
+              </div>
+            )}
+          </div>
 
-                    {/* <div className="funding-logos">
+          {/* <div className="funding-logos">
             <div className="funder-logo"></div>
             <div className="funder-logo"></div>
             <div className="funder-logo"></div>
             <div className="funder-logo"></div>
             <div className="funder-logo"></div>
           </div> */}
-                </div>
-                <div className='home-contact'>
-                    <div className='home-address'>
-                        <div className='home-name'>
-                            <div className='bold-18'>Digital Society Lab</div>
-
-                            <div className='media-container'>
-                                <a
-                                    href='https://twitter.com/DigSocietyLab'
-                                    target='_blank'
-                                    rel='noopener noreferrer'
-                                >
-                                    <FaTwitter />
-                                </a>
-                                <a
-                                    href='https://www.facebook.com/DigSocietyLab'
-                                    target='_blank'
-                                    rel='noopener noreferrer'
-                                >
-                                    <FaFacebookF />
-                                </a>
-                            </div>
-                        </div>
-                        <div className='street-address bold-18'>
-                            <p>1280 Main St</p>
-                            <p>WHamilton,</p>
-                            <p>ONL8S4L8</p>
-                        </div>
-                        <div className='contact-methods bold-16'>
-                            <div>Phone</div>
-                            <div>555-555-5555</div>
-                            <div>Email</div>
-                            <div>
-                                <a href='mailto: webmaster@digitalsocietylab.org'>
-                                    webmaster@digitalsocietylab.org
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='stay-updated'>
-                        <h2>Stay updated with the Digital Society Lab</h2>
-                        <form action=''>
-                            <input
-                                id='sign-up'
-                                type='email'
-                                placeholder='email@address.com'
-                                className='regular-14'
-                            />
-
-                            <label sr-only='true' htmlFor='sign-up' />
-                            <button className='regular-14'>SIGN UP</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
         </div>
-    );
+        <div className="home-contact">
+          <div className="home-address">
+            <div className="home-name">
+              <div className="bold-18">Digital Society Lab</div>
+
+              <div className="media-container">
+                <a
+                  href="https://twitter.com/DigSocietyLab"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="https://www.facebook.com/DigSocietyLab"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaFacebookF />
+                </a>
+              </div>
+            </div>
+            <div className="street-address bold-18">
+              <p>1280 Main St</p>
+              <p>WHamilton,</p>
+              <p>ONL8S4L8</p>
+            </div>
+            <div className="contact-methods bold-16">
+              <div>Phone</div>
+              <div>555-555-5555</div>
+              <div>Email</div>
+              <div>
+                <a href="mailto: webmaster@digitalsocietylab.org">
+                  webmaster@digitalsocietylab.org
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="stay-updated">
+            <h2>Stay updated with the Digital Society Lab</h2>
+            <form
+              action="https://buttondown.email/api/emails/embed-subsribe/digitalsocietylab"
+              method="post"
+              target="popupwindow"
+              onSubmit={handleSubmit}
+            >
+              <input
+                id="sign-up"
+                type="email"
+                name="email"
+                placeholder="email@address.com"
+                className="regular-14"
+              />
+              <input type="hidden" name="tag" value="placeholder-signup" />
+              <input type="hidden" value="1" name="embed" />
+
+              <label sr-only="true" htmlFor="sign-up" />
+              <button className="regular-14">SIGN UP</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
