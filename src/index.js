6f98e99c26a307f131f0e02871a4a7f833270eb2
ChristineWest <christinewwwest@gmail.com>
2023-03-31 16:50:10 -0400

    fix space above page headings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-import { AppProvider } from './appContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "./appContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AppProvider>
-                <App />
-            </AppProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
