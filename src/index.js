aedad99fba5c9b6aa7d5ee1e80a6c60be9422627
AL <skininja673@gmail.com>
2023-04-05 14:22:56 -0400

    added base structure for filter task

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
-import { AppProvider } from "./appContext";
-import "./fonts/Non-Commercial/MDSystem/MDSystem-Bold.otf";
-import "./fonts/Non-Commercial/MDSystem/MDSystem-Medium.otf";
-import "./fonts/Non-Commercial/MDSystem/MDSystem-Regular.otf";
-import "./fonts/Non-Commercial/MDSystem/MDSystem-Semibold.otf";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { AppProvider } from './appContext';
+import { FilterProvider } from './filterContext';
+import './fonts/Non-Commercial/MDSystem/MDSystem-Bold.otf';
+import './fonts/Non-Commercial/MDSystem/MDSystem-Medium.otf';
+import './fonts/Non-Commercial/MDSystem/MDSystem-Regular.otf';
+import './fonts/Non-Commercial/MDSystem/MDSystem-Semibold.otf';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <AppProvider>
+                <FilterProvider>
+                    <App />
+                </FilterProvider>
+            </AppProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
---
+++ src/pages/Publications/CustomDropdown.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomDropdown = () => {
+    return <div>CustomDropdown</div>;
+};
+
+export default CustomDropdown;
