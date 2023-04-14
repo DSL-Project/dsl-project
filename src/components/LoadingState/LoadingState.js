15e631903de0e6f941904f418bc7545b9a2d3cee
AL <skininja673@gmail.com>
2023-04-14 13:22:28 -0400

    removed the material loading state ,netlify was complaining

--- src/components/LoadingState/LoadingState.js
+++ src/components/LoadingState/LoadingState.js
@@ -2,45 +2,45 @@
 // npm install @material-ui/core
 // npm install @material-ui/lab
 import React from 'react';
-import Skeleton from '@material-ui/lab/Skeleton';
+// import Skeleton from '@material-ui/lab/Skeleton';
 
 const LoadingState = () => {
-    // return <div className='loader'></div>;
+    return <div className='loader'></div>;
 
-    return (
-        <>
-            <div className='skeleton-container'>
-                <Skeleton
-                    variant='rect'
-                    width={'90%'}
-                    height={'5vh'}
-                    className='skel'
-                    animation='wave'
-                />
-                <Skeleton
-                    variant='rect'
-                    width={'80%'}
-                    height={'20vh'}
-                    className='skel'
-                    animation='wave'
-                />
-                <Skeleton
-                    variant='rect'
-                    width={'80%'}
-                    height={'58vh'}
-                    className='skel'
-                    animation='pulse'
-                />
-                <Skeleton
-                    variant='rect'
-                    width={'90%'}
-                    height={'5vh'}
-                    className='skel'
-                    animation='wave'
-                />
-            </div>
-        </>
-    );
+    // return (
+    //     <>
+    //         <div className='skeleton-container'>
+    //             <Skeleton
+    //                 variant='rect'
+    //                 width={'90%'}
+    //                 height={'5vh'}
+    //                 className='skel'
+    //                 animation='wave'
+    //             />
+    //             <Skeleton
+    //                 variant='rect'
+    //                 width={'80%'}
+    //                 height={'20vh'}
+    //                 className='skel'
+    //                 animation='wave'
+    //             />
+    //             <Skeleton
+    //                 variant='rect'
+    //                 width={'80%'}
+    //                 height={'58vh'}
+    //                 className='skel'
+    //                 animation='pulse'
+    //             />
+    //             <Skeleton
+    //                 variant='rect'
+    //                 width={'90%'}
+    //                 height={'5vh'}
+    //                 className='skel'
+    //                 animation='wave'
+    //             />
+    //         </div>
+    //     </>
+    // );
 };
 
 export default LoadingState;
