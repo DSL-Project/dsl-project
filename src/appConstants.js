a3ebe13dc724b34013d7b717a33b23b97e296b6a
colindamelio <colin.damelio@gmail.com>
2023-05-23 14:32:47 -0400

    formatting

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,49 +1,49 @@
 //query parameters
-const PEOPLE = 'persons';
-const PUBLICATIONS = 'publications';
-const PROJECTS = 'projects';
+const PEOPLE = "persons";
+const PUBLICATIONS = "publications";
+const PROJECTS = "projects";
 // const MEDIA = 'media';
-const STATIC_QUERY = 'homepage';
-const TRAININGS = 'training';
+const STATIC_QUERY = "homepage";
+const TRAININGS = "training";
 
 const navConstants = [
-    {
-        id: 1,
-        name: 'projects',
-        url: '/projects',
-        queryString: PROJECTS,
-    },
-    {
-        id: 2,
-        name: 'programs',
-        url: '/training',
-        queryString: TRAININGS,
-    },
-		{
-				id: 3,
-				name: 'publications',
-				url: '/publications',
-				queryString: PUBLICATIONS,
-		},
-    {
-        id: 4,
-        name: 'people',
-        url: '/people',
-        queryString: PEOPLE,
-    },
-    {
-        id: 5,
-        name: 'contact',
-        url: '/contact',
-        queryString: STATIC_QUERY,
-    },
+	{
+		id: 1,
+		name: "projects",
+		url: "/projects",
+		queryString: PROJECTS,
+	},
+	{
+		id: 2,
+		name: "programs",
+		url: "/training",
+		queryString: TRAININGS,
+	},
+	{
+		id: 3,
+		name: "publications",
+		url: "/publications",
+		queryString: PUBLICATIONS,
+	},
+	{
+		id: 4,
+		name: "people",
+		url: "/people",
+		queryString: PEOPLE,
+	},
+	{
+		id: 5,
+		name: "contact",
+		url: "/contact",
+		queryString: STATIC_QUERY,
+	},
 ];
 
 export {
-    navConstants,
-    STATIC_QUERY,
-    PUBLICATIONS,
-    PROJECTS,
-    TRAININGS,
-    PEOPLE,
+	navConstants,
+	STATIC_QUERY,
+	PUBLICATIONS,
+	PROJECTS,
+	TRAININGS,
+	PEOPLE,
 };
