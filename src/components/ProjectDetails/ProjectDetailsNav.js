9e1b824e95a687b8913d00bf8747022663d4e448
colindamelio <colin.damelio@gmail.com>
2023-05-23 14:49:06 -0400

    remove hyphen from projects bar

--- src/components/ProjectDetails/ProjectDetailsNav.js
+++ src/components/ProjectDetails/ProjectDetailsNav.js
@@ -1,45 +1,42 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { NavHashLink } from 'react-router-hash-link';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { NavHashLink } from "react-router-hash-link";
 
 const ProjectDetailsNav = ({ pathname, subNavLinks, projectCardInfo }) => {
-    return (
-        <nav className='pdnav-container'>
-            <div className='pd-nav'>
-                <div className='pd-path'>
-                    <NavLink
-                        className='regular-14 pathItem1 pd-link'
-                        to={'/projects'}
-                    >
-                        {pathname.split('/')[1]}
-                    </NavLink>
-                    <span className='regular-14 pathItem2 pd-link'>/</span>
-                    <span className='regular-14 pathItem3 pd-link '>
-                        {pathname.split('/')[2]}
-                    </span>
-                </div>
-                <ul className='pd-main-menu'>
-                    {subNavLinks.map((link, id) => {
-                        return (
-                            <li key={id} className='pd-menu-item'>
-                                <NavHashLink
-                                    to={link.url}
-                                    state={projectCardInfo}
-                                    className={({ isactive }) =>
-                                        isactive
-                                            ? 'regular-14 pd-link pd-active'
-                                            : 'regular-14 pd-link pd-inactive'
-                                    }
-                                >
-                                    {link.name}
-                                </NavHashLink>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        </nav>
-    );
+	return (
+		<nav className="pdnav-container">
+			<div className="pd-nav">
+				<div className="pd-path">
+					<NavLink className="regular-14 pathItem1 pd-link" to={"/projects"}>
+						{pathname.split("/")[1]}
+					</NavLink>
+					<span className="regular-14 pathItem2 pd-link">/</span>
+					<span className="regular-14 pathItem3 pd-link ">
+						{projectCardInfo?.title}
+					</span>
+				</div>
+				<ul className="pd-main-menu">
+					{subNavLinks.map((link, id) => {
+						return (
+							<li key={id} className="pd-menu-item">
+								<NavHashLink
+									to={link.url}
+									state={projectCardInfo}
+									className={({ isactive }) =>
+										isactive
+											? "regular-14 pd-link pd-active"
+											: "regular-14 pd-link pd-inactive"
+									}
+								>
+									{link.name}
+								</NavHashLink>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default ProjectDetailsNav;
--- src/utils.js
+++ src/utils.js
@@ -1,52 +1,47 @@
-/*
-  Utility functions, any generic function that accomplishes an abstract task.
-*/
-
 export const printMonthYear = (dateString) => {
-    const date = new Date(dateString);
-    const dateArray = date.toString().split(' ');
-    return `${dateArray[1]} ${dateArray[3]}`;
+	const date = new Date(dateString);
+	const dateArray = date.toString().split(" ");
+	return `${dateArray[1]} ${dateArray[3]}`;
 };
 
 export const getUniqueValues = (data, type) => {
-    let unique = data.map((item) => item[type]);
-    if (type === 'date') {
-        unique = data.map((item) => item[type].split('-')[0]);
-    }
-    if (type === 'authors') {
-        unique = data.map((item) =>
-            item.authors.map((subItem) => subItem.fields.name)
-        );
-        unique = unique.flat();
-    }
-    if (type === 'publicationType') {
-        unique = data.map((item) => {
-            const targetString = item.publicationType;
-            const arr = targetString.split(' ');
+	let unique = data.map((item) => item[type]);
+	if (type === "date") {
+		unique = data.map((item) => item[type].split("-")[0]);
+	}
+	if (type === "authors") {
+		unique = data.map((item) =>
+			item.authors.map((subItem) => subItem.fields.name)
+		);
+		unique = unique.flat();
+	}
+	if (type === "publicationType") {
+		unique = data.map((item) => {
+			const targetString = item.publicationType;
+			const arr = targetString.split(" ");
 
-            for (let i = 0; i < arr.length; i++) {
-                arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-            }
-            const str2 = arr.join(' ');
-            return str2;
-        });
-    }
-    return [...new Set(unique.sort())];
-    // return ['all',...new Set(unique.sort())];
+			for (let i = 0; i < arr.length; i++) {
+				arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+			}
+			const str2 = arr.join(" ");
+			return str2;
+		});
+	}
+	return [...new Set(unique.sort())];
 };
 
 export const dropdownRelatedData = (uniqueItems) => {
-    const tempData = [];
-    uniqueItems.reduce(
-        (acc, curr) => {
-            acc.value = curr;
-            acc.label = curr;
-            tempData.push({ value: acc.value, label: acc.label });
+	const tempData = [];
+	uniqueItems.reduce(
+		(acc, curr) => {
+			acc.value = curr;
+			acc.label = curr;
+			tempData.push({ value: acc.value, label: acc.label });
 
-            return acc;
-        },
-        { value: '', label: '' }
-    );
+			return acc;
+		},
+		{ value: "", label: "" }
+	);
 
-    return tempData;
+	return tempData;
 };
